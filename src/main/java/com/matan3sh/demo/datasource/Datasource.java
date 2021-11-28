770536047899d2885cbb768f3257a84704cef7b4
Matan Shaviro <matan3sh@gmail.com>
2021-11-28 21:51:11 +0200

    Connect to Postgres SQL and create first migration

---
+++ src/main/java/com/matan3sh/demo/datasource/Datasource.java
@@ -0,0 +1,17 @@
+package com.matan3sh.demo.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Datasource {
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSourcePoolMetadata() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+}
---
+++ src/test/java/com/matan3sh/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matan3sh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
